61bc36c1928f147f68e362d5448eb23c2a6a4682
Anthony Tricarico <128626789+anthony-tricarico@users.noreply.github.com>
2025-09-27 12:32:54 +0200

    Delete exercises_book directory

--- exercises_book/ch-01/ex02.c
+++
@@ -1,6 +0,0 @@
-/* We've implicitly assumed that each call to Compure_next_value requires roughly the same amount of 
- * work as the other calls. How would you change your answer to the preceding question if call i = k
- * requires k + 1 times as much work as the call with i = 0? So if the first call (i = 0) requires 2
- * milliseconds, the second call (i = 1) requires 4, the third (i = 2) requires 6, and so on. */
-
-/* Unclear */
