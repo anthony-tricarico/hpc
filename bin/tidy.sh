535fa2706f11e1ab08fd858763743f5de8f7f1df
anthony-tricarico <tricarico672@gmail.com>
2025-10-12 17:30:12 +0200

    feat: improved tidy.sh

--- bin/tidy.sh
+++ bin/tidy.sh
@@ -7,16 +7,30 @@
 # check if everything works correctly
 # echo "This is the current dir: $cur_dir"
 
-touch main.c
-echo "created main.c file"
-touch makefile
-echo "created makefile"
+if [ ! -e main.c ]; then
+    touch main.c
+    echo "created main.c file"
+    # adding the -e flag makes the echo interpret backlash escapes
+    echo -e "#include <stdio.h>\n\nint main(void) {\n\treturn 0;\n}" > main.c
+    echo "added content to main.c"
+fi
+
+if [ ! -e start.sh ]; then
+    touch start.sh
+    echo "created start.sh file"
+    echo -e "mpirun -n 4 ./build/my_program" > start.sh 
+    chmod +x ./start.sh
+    echo "added content to start.sh and made it executable"
+fi
+
+if [ ! -e makefile ]; then
+    touch makefile
+    echo "created makefile"
+    echo -e "main: main.c\n\tmpicc main.c -o build/my_program" > makefile
+    echo "added content to makefile"
+fi
+
 mkdir build
 echo "added build directory"
 
-# adding the -e flag makes the echo interpret backlash escapes
-echo -e "#include <stdio.h>\n\nint main(void) {\n\treturn 0;\n}" > main.c
-echo "added content to main.c"
-echo -e "main: main.c\n\tmpicc main.c -o build/my_program" > makefile
-echo "added content to makefile"
 echo "All done. Happy coding!"
